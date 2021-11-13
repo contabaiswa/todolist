28278ae36704d9a6f4d8c2e3566d64ceba442f11
Maarten <contabaiswa@outlook.com>
2021-11-13 17:04:06 +0100

    Add todo submit option

--- app.js
+++ app.js
@@ -1,16 +1,33 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+var items = ['Buy food', 'Cook food', 'Eat food'];
+var newItem = "";
 
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post('/', function (req, res) {
+    // newItem = '<li>' + req.body.newItem + '</li>';
+    items.push(req.body.newItem);
+    res.redirect('/');
+});
 
 app.get('/', function (req, res) {
-    const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday'];
-    var date = new Date;
-    var dayNumber = date.getDay();
-    var day = days[dayNumber];
-    // console.log(day);
+    // const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday','Saturday'];
+    var today = new Date;
+    // var dayNumber = today.getDay();
+    // var day = days[dayNumber];
 
-    res.render('list', {weekday: day});
+    var options = {
+        weekday: 'long',
+        day: 'numeric',
+        month: 'long'
+    };
+
+    var day = today.toLocaleDateString('nl-NL', options)
+
+    res.render('list', {weekday: day, listItems: items});
 });
 
 
